ff6dbbd07ff2a56e9c0941664627cd6d901b7225
egor <yegorkrava>
2019-03-01 00:09:20 +0200

    react (internal state of the component, unidirectional data stream, bindings, event handler)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Kravatka',
+        num_comments: 1,
+        points: 1,
+        objectID: 0,
+    },
+    {
+        title: 'React 2',
+        url: 'https://reactjs.org/',
+        author: 'Kravatka 2',
+        num_comments: 2,
+        points: 3,
+        objectID: 1,
+    },
+    {
+        title: 'React 3',
+        url: 'https://reactjs.org/',
+        author: 'Kravatka 3',
+        num_comments: 3,
+        points: 4,
+        objectID: 2,
+    }
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            list: list,
+        };
+
+        this.onDismiss = this.onDismiss.bind(this);
+    }
+
+    onDismiss(id) {
+        const isNotId = item => item.objectID !== id;
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({ list: updatedList });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.list.map(item =>
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title} </a>
+                            </span>
+                            <span>{item.author} </span>
+                            <span>{item.num_comments} </span>
+                            <span>{item.points} </span>
+                            <span>
+                                <button className="btn btn-primary btn-sm" onClick={() => this.onDismiss(item.objectID)} type="button">
+                                Отбросить
+                                </button>
+                            </span>
+                        </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
